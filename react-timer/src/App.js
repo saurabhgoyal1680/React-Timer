0275a6b976ac5966ac4fee8480d598d7efc7f336
saurabhgoyal1680 <59084561+saurabhgoyal1680@users.noreply.github.com>
2019-12-23 14:40:10 +0530

    front End Part (#1)

--- react-timer/src/App.js
+++ react-timer/src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPause, faPlay, faStop, faSyncAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+library.add(faPause, faPlay, faStop, faSyncAlt);
+
+class App extends React.Component{
+	render(){
+		return (
+			<div className="mainDiv">
+				<div className="timer">
+					<div className="time">
+						<div className="runningTime">Hello</div>
+						<input></input>
+					</div>
+					<div className="btn-holder">
+						<button className="leftBtn play"><FontAwesomeIcon icon={faPlay} /></button>
+						<button className="leftBtn stop"><FontAwesomeIcon icon={faStop} /></button>
+						<button className="rightBtn refresh"><FontAwesomeIcon icon={faSyncAlt} /></button>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default App;
